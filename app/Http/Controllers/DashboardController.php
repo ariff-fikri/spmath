3bbdfc51ca4a613448eb3754b9a968d43c64b02d
ariff <a.Fikri@hitowerit.com>
2023-11-30 18:16:05 +0800

    added docblock to all controllers

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,18 +4,37 @@ namespace App\Http\Controllers;
 
 use App\Models\Chapter;
 use App\Models\StudentYear;
+use Illuminate\Contracts\View\View;
 
 class DashboardController extends Controller
 {
-    public function index()
+    /**
+     * Display the student dashboard index.
+     *
+     * @return \Illuminate\Contracts\View\View
+     */
+    public function index(): View
     {
-        $student_year = StudentYear::all();
-        $chapters_form_4 = Chapter::where('student_year_id', 4)->limit(3)->get();
-        $chapters_form_5 = Chapter::where('student_year_id', 5)->limit(3)->get();
+        $studentYears = StudentYear::all();
 
-        $chapters_count_form_4 = Chapter::where('student_year_id', 4)->count();
-        $chapters_count_form_5 = Chapter::where('student_year_id', 5)->count();
+        $chapters = Chapter::with('studentYear')
+            ->whereIn('student_year_id', [4, 5])
+            ->orderBy('student_year_id')
+            ->limit(3)
+            ->get();
 
-        return view('student.dashboard.index', compact('student_year', 'chapters_form_4', 'chapters_form_5', 'chapters_count_form_4', 'chapters_count_form_5'));
+        $chaptersForm4 = $chapters->where('studentYear.id', 4)->values();
+        $chaptersForm5 = $chapters->where('studentYear.id', 5)->values();
+
+        $chaptersCountForm4 = $chapters->where('studentYear.id', 4)->count();
+        $chaptersCountForm5 = $chapters->where('studentYear.id', 5)->count();
+
+        return view('student.dashboard.index', compact(
+            'studentYears',
+            'chaptersForm4',
+            'chaptersForm5',
+            'chaptersCountForm4',
+            'chaptersCountForm5'
+        ));
     }
 }
--- app/Http/Controllers/SpmMcqController.php
+++ app/Http/Controllers/SpmMcqController.php
@@ -5,83 +5,122 @@ namespace App\Http\Controllers;
 use App\Models\Chapter;
 use App\Models\SpmMcq;
 use App\Models\SpmMcqQuestion;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class SpmMcqController extends Controller
 {
-    public function index(SpmMcq $spm_mcq)
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\SpmMcq  $spmMcq
+     * @return \Illuminate\Contracts\View\View
+     */
+    public function index(SpmMcq $spmMcq): View
     {
-        $spm_mcq = SpmMcq::where('id', $spm_mcq->id)->with('quiz_questions')->first();
+        $spmMcq = SpmMcq::where('id', $spmMcq->id)->with('quizQuestions')->first();
 
-        return view('student.spm-mcq.index', compact('spm_mcq'));
+        return view('student.spm-mcq.index', compact('spmMcq'));
     }
 
-    public function spm_mcq()
+    /**
+     * Display a set of randomly selected SpmMcq questions for a multiple-choice quiz.
+     *
+     * @return \Illuminate\Contracts\View\View
+     */
+    public function spm_mcq(): View
     {
         $quiz_questions = SpmMcqQuestion::inRandomOrder()->limit(15)->get();
 
         return view('student.spm-mcq.mcq', compact('quiz_questions'));
     }
 
-    public function create()
+    /**
+     * Display the form for creating a new resource.
+     *
+     * @return \Illuminate\Contracts\View\View
+     */
+    public function create(): View
     {
         $chapters = Chapter::all();
 
-        return view('student.spm-mcq.create', compact('chapters'));
+        return view('student.spm-mcq.create', ['chapters' => $chapters]);
     }
 
-    public function store(Request $request)
+    /**
+     * Store a new SpmMcq and redirect to the quiz creation page.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(Request $request): RedirectResponse
     {
-        $quiz_check = SpmMcq::where('title', $request->title)->first();
+        // Validation
+        $request->validate([
+            'title' => [
+                'required',
+                'string',
+                'max:255',
+                Rule::unique('spm_mcqs', 'title'),
+            ],
+        ]);
 
-        if ($quiz_check) {
-            return back()->with('error', 'Quiz for this chapter has already been submitted. Please try another chapter.');
-        }
-
-        $quiz = SpmMcq::create($request->all());
+        $quiz = SpmMcq::firstOrCreate(['title' => $request->title], $request->except('title'));
 
         return redirect()->route('spm_mcq.create-after-submit', $quiz->id)->with('success', 'Quiz has been stored. Please then enter your questions.');
     }
 
-    public function submit_answer(Request $request, SpmMcq $spm_mcq)
+    /**
+     * Process submitted answers for the specified SpmMcq.
+     *
+     * @param  Request  $request
+     * @param  SpmMcq  $spm_mcq
+     * @return \Illuminate\Contracts\View\View
+     */
+    public function submit_answer(Request $request, SpmMcq $spm_mcq): View
     {
-        $request->session()->remove('spm_mcq');
-        $request->session()->remove('total_correct_answer');
-        $request->session()->remove('total_questions');
+        $request->validate([
+            'question.*' => 'required', // Add more validation rules as needed
+        ]);
 
-        $total_correct_answer = 0;
-        $total_questions = 0;
+        $questions = $spm_mcq->questions;
 
-        foreach ($request->question as $key => $question) {
-            $quiz_question = SpmMcqQuestion::where('id', $key)->first();
+        $quizResult = [
+            'totalCorrectAnswer' => 0,
+            'totalQuestions' => $questions->count(),
+            'questions' => [],
+        ];
 
-            $request->session()->put('quiz.question_' . $key, (object) ([
-                'input_answer' => $question,
-                'input_answer_label' => $quiz_question->answer_label($question, $quiz_question->id),
-                'title' => $quiz_question->title,
-                'correct_answer' => $quiz_question->correct_answer,
-                'correct_answer_label' => $quiz_question->answer_label($question, $quiz_question->id),
-                'status' => ($question == $quiz_question->correct_answer ? true : false),
-            ]));
+        foreach ($questions as $question) {
+            $userAnswer = $request->question[$question->id];
+            $isCorrect = $userAnswer == $question->correct_answer;
 
-            if ($question == $quiz_question->correct_answer) {
-                $total_correct_answer++;
+            $quizResult['questions'][] = [
+                'inputAnswer' => $userAnswer,
+                'inputAnswerLabel' => $question->answer_label($userAnswer, $question->id),
+                'title' => $question->title,
+                'correctAnswer' => $question->correct_answer,
+                'correctAnswerLabel' => $question->answer_label($question->correct_answer, $question->id),
+                'status' => $isCorrect,
+            ];
+
+            if ($isCorrect) {
+                $quizResult['totalCorrectAnswer']++;
             }
-
-            $total_questions++;
         }
 
-        $request->session()->put('total_correct_answer', $total_correct_answer);
-        $request->session()->put('total_questions', $total_questions);
-
-        $quiz_result = (object) ($request->session()->all());
-
-        // dd($quiz_result);
-
-        return view('student.spm-mcq.result', compact('spm_mcq', 'quiz_result'));
+        return view('student.spm-mcq.result', compact('spm_mcq', 'quizResult'));
     }
 
-    public function submit_answer_mcq(Request $request)
+    /**
+     * Process submitted answers for multiple-choice questions.
+     *
+     * @param  Request  $request
+     * @return \Illuminate\Contracts\View\View
+     */
+    public function submit_answer_mcq(Request $request): View
     {
         $request->session()->remove('quiz');
         $request->session()->remove('total_correct_answer');
@@ -91,19 +130,19 @@ class SpmMcqController extends Controller
         $total_correct_answer = 0;
         $total_questions = 0;
 
-        foreach ($request->question as $key => $question) {
-            $quiz_question = SpmMcqQuestion::where('id', $key)->first();
+        foreach ($request->question as $key => $userAnswer) {
+            $quiz_question = SpmMcqQuestion::findOrFail($key);
 
-            $request->session()->put('quiz.question_' . $key, (object) ([
-                'input_answer' => $question,
-                'input_answer_label' => $quiz_question->answer_label($question, $quiz_question->id),
+            $request->session()->put('quiz.question_' . $key, (object) [
+                'input_answer' => $userAnswer,
+                'input_answer_label' => $quiz_question->answer_label($userAnswer, $quiz_question->id),
                 'title' => $quiz_question->title,
                 'correct_answer' => $quiz_question->correct_answer,
-                'correct_answer_label' => $quiz_question->answer_label($question, $quiz_question->id),
-                'status' => ($question == $quiz_question->correct_answer ? true : false),
-            ]));
+                'correct_answer_label' => $quiz_question->answer_label($userAnswer, $quiz_question->id),
+                'status' => ($userAnswer == $quiz_question->correct_answer),
+            ]);
 
-            if ($question == $quiz_question->correct_answer) {
+            if ($userAnswer == $quiz_question->correct_answer) {
                 $total_correct_answer++;
             }
 
@@ -114,61 +153,112 @@ class SpmMcqController extends Controller
         $request->session()->put('total_questions', $total_questions);
         $request->session()->put('time', $request->time);
 
-        $quiz_result = (object) ($request->session()->all());
-
-        // dd($quiz_result);
+        $quiz_result = (object) $request->session()->all();
 
         return view('student.spm-mcq.result', compact('quiz_result'));
     }
 
-    public function edit(Request $request, $quiz)
+    /**
+     * Display the form for editing the specified quiz.
+     *
+     * @param  Request  $request
+     * @param  int  $quiz
+     * @return \Illuminate\View\View
+     */
+    public function edit(Request $request, $quiz): View
     {
-        $quiz = SpmMcq::where('id', $quiz)->with('quiz_questions')->first();
+        $quiz = SpmMcq::where('id', $quiz)->with('quiz_questions')->firstOrFail();
 
         return view('student.spm-mcq.edit', compact('quiz'));
     }
 
-    public function update(Request $request, SpmMcq $spm_mcq)
+    /**
+     * Update the specified quiz in storage.
+     *
+     * @param  Request  $request
+     * @param  SpmMcq  $spm_mcq
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function update(Request $request, SpmMcq $spm_mcq): RedirectResponse
     {
-        $spm_mcq = $spm_mcq->update($request->all());
+        $spm_mcq->update($request->all());
 
-        return back()->with('success', 'Quiz has been updated.');
+        return redirect()->back()->with('success', 'Quiz has been updated.');
     }
 
-    public function update_question(Request $request, SpmMcqQuestion $spm_mcq_question)
+    /**
+     * Update the specified quiz question in storage.
+     *
+     * @param  Request  $request
+     * @param  SpmMcqQuestion  $spm_mcq_question
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function update_question(Request $request, SpmMcqQuestion $spm_mcq_question): RedirectResponse
     {
-        $spm_mcq_question = $spm_mcq_question->update($request->all());
+        $spm_mcq_question->update($request->all());
 
-        return back()->with('success', 'Quiz Question has been updated.');
+        return redirect()->back()->with('success', 'Quiz Question has been updated.');
     }
 
-    public function edit_question(Request $request, SpmMcqQuestion $spm_mcq_question)
+    /**
+     * Display the form for editing the specified quiz question.
+     *
+     * @param  Request  $request
+     * @param  SpmMcqQuestion  $spm_mcq_question
+     * @return \Illuminate\View\View
+     */
+    public function edit_question(Request $request, SpmMcqQuestion $spm_mcq_question): View
     {
         return view('student.spm-mcq.edit-question', compact('spm_mcq_question'));
     }
 
-    public function show()
+    /**
+     * Display a listing of quizzes.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function show(): View
     {
         $quizzes = SpmMcq::all();
 
         return view('student.spm-mcq.show', compact('quizzes'));
     }
 
-    public function create_after_submit(Request $request, $quiz)
+    /**
+     * Display the form for creating questions after submitting a quiz.
+     *
+     * @param  Request  $request
+     * @param  int  $quiz
+     * @return View
+     */
+    public function create_after_submit(Request $request, int $quiz): View
     {
         $quiz = SpmMcq::where('id', $quiz)->with('quiz_questions')->first();
 
         return view('student.spm-mcq.create-question-after-submit', compact('quiz'));
     }
 
-    public function question_store(Request $request)
+    /**
+     * Store a newly created quiz question in storage.
+     *
+     * @param  Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function question_store(Request $request): RedirectResponse
     {
-        $quiz_question = SpmMcqQuestion::create($request->all());
+        SpmMcqQuestion::create($request->all());
 
         return back()->with('success', 'Quiz Question has been stored.');
     }
 
-    public function remove(Request $request, SpmMcq $spm_mcq)
+    /**
+     * Remove the specified SPM MCQ from storage.
+     *
+     * @param  Request  $request
+     * @param  SpmMcq  $spm_mcq
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function remove(Request $request, SpmMcq $spm_mcq): RedirectResponse
     {
         $spm_mcq->delete();
 
